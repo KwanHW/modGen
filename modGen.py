c872164b5f571b7a9028982d3a72bd655052ecfe
KwanHW <kwanhaowei@gmail.com>
2020-07-28 12:31:50 +0800

    Automatically creates directory for the new semester Combined code into 1 file

--- modGen.py
+++ modGen.py
@@ -1,20 +1,59 @@
 #! python 3
 
 from icalendar import Calendar
-from mod import Module
 import os
 import shutil
+
+class Module():
+    def __init__(self, modCode, modName):
+        self.code = modCode
+        self.name = modName
+        self.lessons = []
+
+def createYearDir(dirArr):
+    yearNum = len(dirArr) + 1
+    os.makedirs(f'Year {yearNum}', exist_ok=True)
+    dirArr.append(f'Year {yearNum}')
+    print(f'Made a Year {yearNum} Directory')
+
+def createEmptySem(root):
+    # Gets all the Year directories in root
+    dirArr = sorted([i for i in os.listdir(root) if (os.path.isdir(i) and 'Year' in i)])
+    print(dirArr)
+
+    # If there are no year directories
+    if not (dirArr):
+        createYearDir(dirArr)
+
+    for yrDir in dirArr:
+        # Checks current year if semester directories exist
+        semDir = os.listdir(yrDir)
+        if len(semDir)< 2:
+            newSemDir = os.path.join('.',yrDir,f'Sem {len(semDir)+1}')
+            os.makedirs(newSemDir)
+            print(f'Make module directories in {newSemDir}')
+            return newSemDir
+
+        # Both semester directories are in
+        else:
+            # Walks down the year directories and check if they are not empty
+            for dirPath, dirNames, _ in sorted(os.walk(yrDir)):
+                # If a semester directory is empty, setup module directories in there
+                if 'Sem' in os.path.basename(dirPath) and not(dirNames):
+                    print(f'Make module directories in {dirPath}')
+                    return newSemDir
+            else:
+                # All semester directories have modules, create a new year directory
+                createYearDir(dirArr)
 
 # TODO: Probably change the input in future
 FILE_PATH = 'nusmods_calendar.ics'
 MOD_DICT = {}
 ROOT_DIR = os.getcwd()
 
-calFile = open(FILE_PATH, 'rb')
 
-cal = Calendar.from_ical(calFile.read())
-
-calFile.close()
+with open(FILE_PATH, 'rb') as calFile:
+    cal = Calendar.from_ical(calFile.read())
 
 # Gather the module information from the ical file
 for comp in cal.walk():
@@ -35,26 +74,21 @@ for comp in cal.walk():
         if not(lessonType in MOD_DICT[modCode].lessons):
             MOD_DICT[modCode].lessons.append(lessonType)
 
+targetSem = createEmptySem(ROOT_DIR)
+
 # Creating the module directories
 for key in MOD_DICT.keys():
-    os.makedirs(key)
+    os.makedirs(os.path.join(targetSem,key))
     
 # Creating the subdirectories for each module
-for dirPath, dirNames, fileNames in os.walk("."):
+for dirPath, dirNames, fileNames in os.walk(targetSem):
     mod = os.path.basename(dirPath)
 
     # Checks if the directory is the module
     if not(mod in MOD_DICT.keys()):
         continue
-
-    # Creates the subdirectory for each module
-    lessonPath = os.path.join(dirPath, lesson)
     
     # Creates a directory for each lesson type 
-    [os.makedirs(lessonPath) for lesson in MOD_DICT[mod].lessons]
+    [os.makedirs(os.path.join(dirPath, lesson)) for lesson in MOD_DICT[mod].lessons]
 
-# ! FOR TESTING ONLY
-# ! Deletes the directory & subdirectories after whole script has executed
-for key in MOD_DICT.keys():
-    shutil.rmtree(os.path.join('.',key))
 
